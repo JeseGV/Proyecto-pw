f0f907fadb0deee3fac67a32f915d180d25a5cf3
JeseGV <65308030+JeseGV@users.noreply.github.com>
2022-10-31 11:40:26 -0700

    inicio del git

---
+++ scr.js
@@ -0,0 +1,8 @@
+
+function ver() {
+    if(document.getElementById("floatingInput").value == "hola@tec2.mx"){
+        if(document.getElementById("floatingPassword").value == "1234"){
+            open("inicio.html")
+        }
+    }
+}
