255facff7b6d663cfc93f95057be2fefc00132fc
JeseGV <d.villegas1579@gmail.com>
2022-10-31 19:20:00 -0700

    io

--- scr.js
+++ scr.js
@@ -6,7 +6,7 @@ var producto = "";
 function ver() {
     if(document.getElementById("floatingInput").value == "hola@tec2.mx"){
         if(document.getElementById("floatingPassword").value == "1234"){
-            open("inicio.html")
+            document.open("inicio.html")
         }
     }
 }
